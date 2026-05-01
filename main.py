5edd5d0724f56b3d93b2460294ddb2a0232b7238
raespe81 <59703248+raespe81@users.noreply.github.com>
2026-05-01 15:57:35 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Lista de cuentas de MT5 permitidas
+CUENTAS_VIP = ["1234567", "9876543"]
+
+@app.get("/validar")
+def validar(cuenta: str):
+    if cuenta in CUENTAS_VIP:
+        return {"status": "autorizado"}
+    return {"status": "denegado"}
