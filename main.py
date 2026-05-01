41cfda7d70baeb2cbed4b0d9dcc433f90c599cb3
raespe81 <59703248+raespe81@users.noreply.github.com>
2026-05-01 16:54:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # Lista de cuentas de MT5 permitidas
-CUENTAS_VIP = ["1234567", "9876543"]
+CUENTAS_VIP = ["1234567", "34221750"]
 
 @app.get("/validar")
 def validar(cuenta: str):
